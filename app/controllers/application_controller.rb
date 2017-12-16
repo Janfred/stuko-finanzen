f036af49aaef44b0ed3bf8e2fa493c43c1757c16
Jan-Frederik Rieckers <rieckers@uni-bremen.de>
2017-12-16 11:51:09 +0100

    [DEVEL-COMMIT] create user in seed and automatically log in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :development_sign_in
+
+  private
+
+  def development_sign_in
+    sign_in User.first
+  end
 end
